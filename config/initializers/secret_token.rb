f41ad94e661dc95162c0df5af39ac08e68dcb1e5
stickylynch13 <aklynch11us@gmail.com>
2013-05-28 18:18:39 -0400

    Initial Commit and junk.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magpie::Application.config.secret_token = '6a48b84ae78a762cbea258573b5ee8a0c59a04d664ba756d0209883cfde77fd678750f2e0bc5ef5fb6d5948e8209d0fdf3e28fae6d6af811752f05d652c78ba1'
